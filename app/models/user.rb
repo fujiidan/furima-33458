85923ef23f8779ddff3ff4eb05c293e78b3224f2
fujiidan <2607dan@gmail.com>
2020-12-29 23:01:07 +0900

    新規登録、ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :encrypted_password, format: {with: /\A[a-z0-9]+\z/i, message: "Include both letters and numbers"} 
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers"} 
        
   with_options presence: true do
     validates :nickname
