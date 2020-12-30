5ead096bd455be0387c6f74a2d961b82b78376ed
fujiidan <2607dan@gmail.com>
2020-12-30 11:34:20 +0900

    FactoryBot　userの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::}
+    transient do
+      person {Gimei.unique.name}  
+    end
+
+    nickname {person}
+    first_name               {person.first.kanji}
+    last_name                {person.last.kanji}
+    first_name_kana          {person.first.katakana}
+    last_name_kana           {person.last.katakana}
+    email                    {Faker::Internet.free_email}
+    password                 {Faker::Internet.password(min_length: 6)}
+    password_confirmation    {password}
+    user_birth_date          {Faker::Date.in_date_period}
   end
 end
