ab607f457e1d18b3a5f932878493c0b6f1e5c7a2
fujiidan <2607dan@gmail.com>
2021-01-01 14:47:16 +0900

    categoryモデル作成　アソシエーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 
   with_options presence: true do
     validates :image
