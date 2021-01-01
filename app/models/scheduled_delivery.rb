3a2ab4145c07ce86450b270d3052685ec058403f
fujiidan <2607dan@gmail.com>
2021-01-01 15:12:05 +0900

    scheduled_deliveryモデル作成

---
+++ app/models/scheduled_delivery.rb
@@ -0,0 +1,9 @@
+class ScheduledDelivery < ActiveHash::Base
+
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '１〜２日で発送' },
+    { id: 2, name: '２〜３日で発送' },
+    { id: 2, name: '４〜７日で発送' }
+  ]
+end
