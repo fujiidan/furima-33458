411ef2f0cfc7d273f559305c0226f9cd4638fd3a
fujiidan <2607dan@gmail.com>
2021-01-01 14:28:08 +0900

    itemモデルバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,25 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: {maximum: 40 }
+    validates :text, length: {maximum: 10000 }
+    validates :price, numericality: {only_integer: true, message: "Half-width number" }
+  end
+
+  with options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  validates :price, numericality: {
+    greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+
 end
