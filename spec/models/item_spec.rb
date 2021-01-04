a34973a9da082775adf682e549a38844a6d9c4eb
fujiidan <2607dan@gmail.com>
2021-01-04 11:05:38 +0900

    item単体テストコードのバリデーションの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,8 +34,20 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Category Select')
     end
 
+    it 'カテゴリーの選択が０では登録できないこと' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category Select')
+    end
+
     it '商品の状態についての情報が必須であること' do
       @item.sales_status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Sales status Select')
+    end
+
+    it '商品の状態の選択が0では登録できないこと' do
+      @item.sales_status_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include('Sales status Select')
     end
@@ -46,14 +58,32 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Shipping fee status Select')
     end
 
+    it '配送料の負担の選択が0では登録できないこと' do
+      @item.shipping_fee_status_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping fee status Select')
+    end
+
     it '発送元の地域についての情報が必須であること' do
       @item.prefecture_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include('Prefecture Select')
     end
 
+    it '発送元の地域の選択が0では登録できないこと' do
+      @item.prefecture_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Prefecture Select')
+    end
+
     it '発送までの日数についての情報が必須であること' do
       @item.scheduled_delivery_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Scheduled delivery Select')
+    end
+
+    it '発送までの日数の選択が0では登録できないこと' do
+      @item.scheduled_delivery_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include('Scheduled delivery Select')
     end
