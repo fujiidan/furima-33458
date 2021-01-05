2d888b53d9cd84421b8b8f6b3997c7075e0f9a49
fujiidan <2607dan@gmail.com>
2021-01-05 09:32:20 +0900

    items/editアクションへのアクセス制限設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -43,4 +44,11 @@ class ItemsController < ApplicationController
       :prefecture_id, :scheduled_delivery_id, :price
     ).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != @item.user_id
+      redirect_to root_path
+    end  
+  end  
 end
