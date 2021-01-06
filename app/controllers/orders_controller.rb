fc758bfec4a4dc5802ab30ce7d4ef5316c1a7511
fujiidan <2607dan@gmail.com>
2021-01-06 13:04:50 +0900

    サーバー側のapi処理の実装及び購入処理実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,20 @@
 class OrdersController < ApplicationController
-  
+  require 'payjp'
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
   
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
+      Payjp.api_key = "sk_test_007aa8bab87a451f5db8c97d"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_address_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -21,6 +28,6 @@ class OrdersController < ApplicationController
   def order_address_params
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(
-      user_id: current_user.id, item_id: params[:item_id])
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end    
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}[-][0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id, numericality: {other_than: 0, message: "Select"}
     validates :city
     validates :addresses
-    validates :phone_number, format: {with: /\A\d{10, 11}\z/i, message: "input only number"}
+    validates :phone_number, numericality: {only_integer: true, message: "input only number"}, length: {in: 10..11 }
+    validates :token
   end
 
   def save
-    oreder = Order.create(user_id: current_user.id, item_id: params[:item_id])
-    addresses.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
                      building: building, addresses: addresses, phone_number: phone_number, order_id: order.id)
   end
 
