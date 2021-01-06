b614c82de0f894ec61e46b0b35a8d7fb4626e22e
fujiidan <2607dan@gmail.com>
2021-01-06 14:40:09 +0900

    ordersコントローラにorders/indexのアクセス制限の実施

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: :index
+  before_action :find_item
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
   
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       pay_item
@@ -34,4 +35,12 @@ class OrdersController < ApplicationController
     )
   end
 
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end  
 end
